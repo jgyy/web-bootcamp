42a29a6963d1bba477c33081d3e99dc079083abb
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2021-07-08 18:38:08 +0800

    done with const and var

---
+++ 14-javascript/14/constVar.js
@@ -0,0 +1,10 @@
+let asdf = 12;
+const luckyNum = 26;
+console.log(asdf);
+console.log(luckyNum);
+const feetInMiles = 5280;
+console.log(feetInMiles);
+
+var runDistance = 26.2;
+runDistance += 1;
+console.log(runDistance);
---
+++ 14-javascript/14/updateVar.js
@@ -0,0 +1,26 @@
+let score = 0;
+score = 5;
+score = score + 5;
+score += 5;
+console.log(score);
+score += 45;
+console.log(score);
+score += 10;
+score -= 50;
+score *= 2;
+console.log(score);
+score /= 2;
+score += 2;
+score -= 2;
+console.log(score);
+
+let numLives = 9;
+numLives -= 1;
+numLives--
+console.log(numLives);
+numLives--
+console.log(numLives);
+numLives++
+console.log(numLives);
+numLives += 9;
+console.log(numLives);
