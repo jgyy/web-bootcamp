c2c749f5bda805e756dc260a6e7c791a112adc3f
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2021-07-09 11:32:55 +0800

    done with methods with argumentd

---
+++ 14-javascript/15/strArgs.js
@@ -0,0 +1,15 @@
+console.log("asdfghjkl".toUpperCase());
+let msg = "haha that is so funny!";
+console.log(msg);
+console.log(msg.indexOf("h"));
+console.log(msg.indexOf("!"));
+console.log(msg.indexOf("$"));
+console.log(msg.indexOf("is"));
+console.log(msg.slice(5));
+console.log(msg.slice(5, 9));
+console.log(msg.slice(5, 10));
+console.log(msg.slice(10, 12));
+console.log(msg.slice(-6));
+console.log(msg.replace("haha", "lolololol"));
+console.log(msg.replace("h", "H"));
+console.log("lol".repeat(10));
