1fbd72f29b7836fad7921c7bc1c622cd862c5ec1
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2021-07-09 09:52:45 +0800

    finished with variable names

---
+++ 14-javascript/14/bool.js
@@ -0,0 +1,9 @@
+console.log(true);
+console.log(false);
+let isActiveGame = true;
+isActiveGame = false;
+console.log(isActiveGame);
+let isLoggedIn = true;
+console.log(isLoggedIn);
+isLoggedIn = 123456;
+console.log(isLoggedIn);
---
+++ 14-javascript/14/varName.js
@@ -0,0 +1,6 @@
+let _age = 33;
+console.log(_age);
+let currentYear = 2021;
+console.log(currentYear);
+let n = 9;
+console.log(n);
