83d6b653e6b5c4342d9e5ea9a6c2b6d9ff96bc91
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2021-07-15 11:56:55 +0800

    practiced shift and unshift

---
+++ 14-javascript/17/shift.js
@@ -0,0 +1,18 @@
+let movieLine = ["tom", "nancy", "pablo", "oliver", "eva"];
+console.log(movieLine);
+console.log(movieLine.shift());
+console.log(movieLine);
+let nextPatron = movieLine.shift();
+console.log(nextPatron);
+console.log(movieLine);
+movieLine.push("colt");
+movieLine.shift();
+movieLine.shift();
+movieLine.shift();
+movieLine.shift();
+console.log(movieLine);
+movieLine.push("jerry", "deniz", "kevin", "carly");
+console.log(movieLine);
+movieLine.unshift("VIP");
+console.log(movieLine);
+movieLine.shift();
