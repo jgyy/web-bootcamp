6fc72e7d4c0eea6c9985d8950316b128c8d6e89d
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2021-07-15 15:47:58 +0800

    added object literal

---
+++ 14-javascript/18/obj.js
@@ -0,0 +1,8 @@
+const person = { firstName: "Mick", lastName: "Jagger" };
+console.log(person);
+const kitchenSink = {
+  favNum: 92319023,
+  isFunny: true,
+  colors: ["red", "orange"],
+};
+console.log(kitchenSink);
