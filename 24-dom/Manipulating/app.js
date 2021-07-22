17fc77ab8e03f098556a508ae3e187f312698da8
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2021-07-22 15:53:27 +0800

    done with changing style

--- 24-dom/Manipulating/app.js
+++ 24-dom/Manipulating/app.js
@@ -6,5 +6,11 @@ for (let link of allLinks) {
   linkText.splice(index, 0, link.innerHTML);
 }
 for (let i = 0; i < allLinks.length; i++) {
+  let color = "#";
+  for (let j = 0; j < 8; j++) {
+    const num = Math.floor(Math.random() * 16);
+    color += num.toString(16);
+  }
   allLinks[i].innerHTML = linkText[i];
+  allLinks[i].style.color = color;
 }
