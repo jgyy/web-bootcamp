0597b0ff6e974615697fe35de42a01ebe5ebdd1b
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2021-07-22 17:58:23 +0800

    done with pokemon sprites

--- 24-dom/Manipulating/app.js
+++ 24-dom/Manipulating/app.js
@@ -1,16 +1,22 @@
 const allLinks = document.querySelectorAll("a");
 const linkText = [];
 
+function color() {
+  let color = "#";
+  for (let j = 0; j < 6; j++) {
+    const num = Math.floor(Math.random() * 16);
+    color += num.toString(16);
+  }
+  return color;
+}
+
 for (let link of allLinks) {
   index = Math.floor(Math.random() * linkText.length);
   linkText.splice(index, 0, link.innerHTML);
 }
+
 for (let i = 0; i < allLinks.length; i++) {
-  let color = "#";
-  for (let j = 0; j < 8; j++) {
-    const num = Math.floor(Math.random() * 16);
-    color += num.toString(16);
-  }
   allLinks[i].innerHTML = linkText[i];
-  allLinks[i].style.color = color;
+  allLinks[i].style.color = color();
+  allLinks[i].style.textDecorationColor = color();
 }
--- 24-dom/Pokemon/app.js
+++ 24-dom/Pokemon/app.js
@@ -1,19 +1,33 @@
 // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
 
-const container = document.querySelector('#container');
-const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+// <section id="container"></section>
+const container = document.querySelector("#container");
+const baseURL =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+const numbers = [];
+for (let i = 1; i <= 898; i++) {
+  const random = Math.floor(Math.random() * numbers.length);
+  numbers.splice(random, 0, i);
+}
 
+for (let i of numbers) {
+  // <div class="pokemon"></div>
+  const pokemon = document.createElement("div");
+  pokemon.classList.add("pokemon");
+  // <span>#500</span>
+  const label = document.createElement("span");
+  label.innerText = `#${i}`;
+  // <img src="url/500.png"></img>
+  const newImg = document.createElement("img");
+  newImg.src = `${baseURL}${i}.png`;
 
-for (let i = 1; i <= 151; i++) {
-    const pokemon = document.createElement('div');
-    pokemon.classList.add('pokemon');
-    const label = document.createElement('span');
-    label.innerText = `#${i}`;
-    const newImg = document.createElement('img');
-    newImg.src = `${baseURL}${i}.png`
-
-
-    pokemon.appendChild(newImg);
-    pokemon.appendChild(label);
-    container.appendChild(pokemon)
+  /*<section id="container">
+    <div class="pokemon">
+      <img src="url/500.png"></img>
+      <span>#500</span>
+    </div> * 898
+  </section>*/
+  pokemon.appendChild(newImg);
+  pokemon.appendChild(label);
+  container.appendChild(pokemon);
 }
